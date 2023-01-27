2bcd7ef661e81ca069941d72e8fcabdb533d7f93
harmfuly <harmfuly7@gmail.com>
2023-01-27 20:39:11 +0200

    feat: task is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+  let currentSymbol;
+  let topSymbol;
+
+  if (str.length % 2 !== 0) return false;
+
+  for (let i = 0; i < str.length; i++) {
+    currentSymbol = str[i];
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      topSymbol = stack[stack.length - 1];
+
+      if (currentSymbol === bracketsConfig[j][1] && topSymbol === bracketsConfig[j][0]) {
+        stack.pop();
+      } else if (currentSymbol === bracketsConfig[j][0]) {
+        stack.push(currentSymbol);
+      }
+    }
+  }
+  
+  return stack.length === 0;
+
 }
